b99f561bd715d63b87262c1abb3bf428ea370a6b
Fazan4ik <jagannath2006@outlook.com>
2023-10-30 16:11:27 +0200

    123

--- CryptoWindow.xaml.cs
+++ CryptoWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -25,11 +26,14 @@ namespace NetworkProgrammingP12
     {
         private readonly HttpClient _httpClient;
         public ObservableCollection<CoinData> CoinsData { get; set; }
+        private ListViewItem? previousSelectedItem;
+            
         public CryptoWindow()
         {
             InitializeComponent();
             CoinsData = new();
             this.DataContext = this;
+            LabelCourse.Content = "Графіки курсів криптовалют";
             _httpClient = new()
             {
                 BaseAddress = new Uri("https://api.coincap.io/")
@@ -56,17 +60,107 @@ namespace NetworkProgrammingP12
             {
                 CoinsData.Add(coinData);
             }
-
         }
-        private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void FrameworkElement_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-           /* if (sender in ListViewItem item){
-                item.Background = Brushes.Aqua;
-            }*/
+            if (sender is ListViewItem item)
+            {
+                if (item.Content is CoinData coinData)
+                {
+                    if (previousSelectedItem is not null)
+                    {
+                        previousSelectedItem.Background = Brushes.White;  
+                    }
+                    // MessageBox.Show(coinData.symbol);
+                    ShowHistory(coinData);
+                    LabelCourse.Content = $"Графік курса криптовалюти {coinData.name}";
+                    item.Background = Brushes.Aqua;
+                    previousSelectedItem = item;
+                    MessageBox.Show($"Asset id -> {coinData.id}");
+
+                }
+                // DrawLine(10, 10, 100, 100);
+            }
+        }
+
+        private async Task ShowHistory(CoinData coinData)
+        {
+            String body = await _httpClient.GetStringAsync(
+                $"/v2/assets/{coinData.id}/history?interval=d1");
+            var response = JsonSerializer.Deserialize<HistoryResponse>(body);
+            
+            if(response == null || response.data == null)
+            {
+                MessageBox.Show("Помилка завантаження данних");
+                return;
+            }
+            double yOffset = 30;
+            double graphH = Graph.ActualHeight - yOffset;
+
+            long minTime, maxTime;
+            double minPrice, maxPrice;
+            minTime = maxTime = response.data[0].time;
+            minPrice = maxPrice = response.data[0].price;
+            foreach(HistoryItem item in response.data)
+            {
+                if (item.time < minTime) { minTime = item.time; }
+                if (item.time > maxTime) { maxTime = item.time; }
+
+                if (item.price < minPrice) { minPrice = item.price; }
+                if (item.price > maxPrice) { maxPrice = item.price; }                
+            }
+            double x0 = (response.data[0].time - minTime) * Graph.ActualWidth / (maxTime - minTime);
+            double y0 = graphH - (response.data[0].price - minPrice) * graphH / (maxPrice - minPrice);
+            Graph.Children.Clear(); 
+            
+            foreach (HistoryItem item in response.data)
+            {
+                double x = (item.time - minTime) * Graph.ActualWidth / (maxTime - minTime);
+                double y = graphH - (item.price - minPrice) * graphH / (maxPrice - minPrice);
+                if (y < y0)
+                {
+                    DrawLine(x0, y0, x, y, Brushes.Green);
+                }
+                else
+                {
+                    DrawLine(x0, y0, x, y, Brushes.Red);
+                }
+                
+                x0 = x;
+                y0 = y;
+            }
+            DrawLine(0, graphH, Graph.ActualWidth, graphH);
+        }
+
+        private void DrawLine(double x1,double y1,double x2,double y2, Brush brush = null)
+        {
+            brush ??= new SolidColorBrush(Colors.Black);
+            Graph.Children.Add(new Line
+            {
+                X1 = x1,
+                Y1 = y1,
+                X2 = x2,
+                Y2 = y2,
+                Stroke = brush,
+                StrokeThickness = 2
+            });
         }
     }
 
     ///////////////// ORM ////////////////////////
+    
+    public class HistoryResponse
+    {
+        public List<HistoryItem> data { get; set; }
+        public long timestamp { get; set; }
+    }
+    public class HistoryItem
+    {
+        public String priceUsd { get; set; }
+        public long time { get; set; }
+        public double price => Double.Parse(priceUsd, CultureInfo.InvariantCulture);
+    }
+
     public class CoincapResponse
     {
         public List<CoinData> data { get; set; }
